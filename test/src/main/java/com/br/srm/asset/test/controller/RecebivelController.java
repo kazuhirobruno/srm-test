8e3d4284e012b4080a00172d6d68a96cb5d499e6
Bruno <brunokazuhiro@gmail.com>
2026-07-23 17:16:15 -0300

    feat: create endpoint for liquidar

--- test/src/main/java/com/br/srm/asset/test/controller/RecebivelController.java
+++ test/src/main/java/com/br/srm/asset/test/controller/RecebivelController.java
@@ -2,11 +2,14 @@ package com.br.srm.asset.test.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.br.srm.asset.test.domain.Recebivel;
+import com.br.srm.asset.test.dtos.CreateLiquidacaoRequestDTO;
 import com.br.srm.asset.test.dtos.CreateRecebivelRequestDTO;
 import com.br.srm.asset.test.service.RecebivelService;
 
@@ -24,5 +27,11 @@ public class RecebivelController {
   public ResponseEntity<Recebivel> cadastrar(@RequestBody CreateRecebivelRequestDTO recebivel) {
     Recebivel novoRecebivel = recebivelService.cadastrarRecebivel(recebivel);
     return ResponseEntity.status(HttpStatus.CREATED).body(novoRecebivel);
+  }
+
+  @PatchMapping("/{id}/liquidar")
+  public ResponseEntity<Void> liquidar(@PathVariable Long id, @RequestBody CreateLiquidacaoRequestDTO request) {
+    recebivelService.liquidar(id, request.getMoedaPagamento(), request.getTaxaBase());
+    return ResponseEntity.noContent().build();
   }
 }
---
+++ test/src/main/java/com/br/srm/asset/test/dtos/CreateLiquidacaoRequestDTO.java
@@ -0,0 +1,15 @@
+package com.br.srm.asset.test.dtos;
+
+import java.math.BigDecimal;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateLiquidacaoRequestDTO {
+  private String moedaPagamento;
+  private BigDecimal taxaBase;
+}
